72cd4584d65d8094d9a678f9a377501873aa6bdd
sameerbasha17 <sksameerbasha703@gmail.com>
2025-06-18 17:09:32 +0530

    Understanding float data type

---
+++ Float_DT.java
@@ -0,0 +1,21 @@
+public class Float_DT {
+    public static void main(String args[])
+    {
+        // Understanding float data type
+        /* Size : 4 bytes (32 bits) 
+         * Values : upto 7 decimal digits
+         * Default value : 0.0f
+        */
+
+      //  float a =1.43; // throughs a error if you declare without the "f" suffix
+        float b = 1.143f;
+        float c = 89.89f;
+        int d =7;
+        
+      //  System.out.println(a);
+        System.out.println(b+c);
+        System.out.println(c+d);
+        
+    }
+        
+}
