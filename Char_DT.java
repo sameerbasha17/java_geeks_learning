a658d372a1749659df834f9d183fc81577f229c9
sameerbasha17 <sksameerbasha703@gmail.com>
2025-06-18 17:38:49 +0530

    Understanding the char data type

---
+++ Char_DT.java
@@ -0,0 +1,17 @@
+public class Char_DT {
+    public static void main(String args[])
+    {
+        // Understanding the Char data type
+        /* Size = 2 bytes(16 bits)
+         * Values= '\u0000' (0)to '\uffff' (65535)
+         * Default value = '\u0000'
+         */
+
+         char a = 'a';
+         char b = 's';
+
+         System.out.println(a); // outputs a
+         System.out.println(a+b); // outputs 212
+    }
+    
+}
