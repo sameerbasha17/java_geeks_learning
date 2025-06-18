49614d24b96d199baa9f2c30ee544d68635bd718
sameerbasha17 <sksameerbasha703@gmail.com>
2025-06-18 13:22:00 +0530

    blank program

---
+++ Blank.java
@@ -0,0 +1,8 @@
+
+
+public class Blank {
+    public static void main(String args[])
+    {
+
+    }                                         // lets see, the main method can work or not without any statements.
+}
