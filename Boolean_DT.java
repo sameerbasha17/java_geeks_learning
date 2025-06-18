c8b9a03575960b790dbafdbf1af9b3de0754ea21
sameerbasha17 <sksameerbasha703@gmail.com>
2025-06-18 13:41:27 +0530

    understanding boolean data type

---
+++ Boolean_DT.java
@@ -0,0 +1,15 @@
+public class Boolean_DT {
+        static boolean c;
+    public static void main(String args[])
+    {
+        boolean a = true;
+        boolean b = false;
+    
+        System.out.println("Are you a boy ? "+a);
+        if(b == false)
+        {
+            System.out.println("Are you lazy ? "+b);
+        }
+        System.out.println("The default value of boolean data type is "+c);
+    }
+}
