6cf308887608ab029f17746e2d1b24baacdfa6ee
sameerbasha17 <sksameerbasha703@gmail.com>
2025-06-20 07:09:33 +0530

    Reference behaviour when two reference variables point same object

--- Referenc_Behavior_NPDT.java
+++ Referenc_Behavior_NPDT.java
@@ -11,8 +11,8 @@ public class Referenc_Behavior_NPDT {
         p1.b =200;
         System.out.println("p1.a = "+p1.a+"   p1.b = "+p1.b); // p1.a = 100   p1.b = 200
         Point2 p2 = p1; //Both p1 and p2 points the same object
-        p2.a =50;
+        p2.a =50;       //Here p1 and p2 are called reference variables
 
-        System.out.println("p1.a = "+p1.a+ "   p2.a = "+p2.a); // p1.a = 50   p2.a = 50
+        System.out.println("p1.a = "+p1.a+ "   p2.a = "+p2.a); // p1.a = 50   p2.a = 50 //Any modifications made through p2 is reflected when accessing via p1
     }
 }
