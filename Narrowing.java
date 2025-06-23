bc0c90786daf3f5b88d9d5bcbc78f690c5b53f83
sameerbasha17 <sksameerbasha703@gmail.com>
2025-06-23 17:34:38 +0530

    Understanding Narrowing

--- Narrowing.java
+++ Narrowing.java
@@ -4,13 +4,23 @@ public class Narrowing {
         // Narrowing or explicit conversion
         // if we want to assign the value of a larger data type to a smaller data type we perform explicit type casting
         // Double -> Float -> long -> int -> short -> byte
-        double d = 2999444445.54898056d;
-        System.out.println("Double value = "+d); // printing the initial value of double variable // it print is scientific notation i.e.:5.874444455489806E8 because if the number is too large (=>1e7) or too small(<=1e-3) it switches to scientific notation automatically
+        double d = 2999444445.94898056d;
+        System.out.println("Double value = "+d); // printing the initial value of double variable // it print is scientific notation i.e.:2.9994444459489803E9 because if the number is too large (=>1e7) or too small(<=1e-3) it switches to scientific notation automatically
         long l = (long)d;
         System.out.println("Explicit type casting (double to long)");
-        System.out.println("long value = "+l);
+        System.out.println("long value = "+l); // prints 2999444445 // fractional part removed(truncated not rounded)
         int i = (int)l;
-        System.out.println("Explicit type casting (long to int)");
-        System.out.println("integer value = "+i);
+        System.out.println("Explicit type casting (long to int)"); 
+        System.out.println("integer value = "+i); // prints -1295522851 // Overflow due to exceeding int limit (2,147,483,647)
+
+
+        //reassigning value to int
+        i = 32001;
+        System.out.println("Explicit type casting (int to short)");
+        short s = (short)i; 
+        System.out.println("Short value : "+s);   // prints 32001
+        System.out.println("Explicit type casting (short to byte)");
+        byte b = (byte)s;
+        System.out.println("Byte value : "+b); // prints 1 i.e.: 32001%256=1 // Overflow due to exceeding byte limit (-128 to127)
     }
 }
